e037f7da479d4a4b5107260b78dc35fbefa44027
ajith-kumar99 <ajithkumarguntipalli@gmail.com>
2025-08-26 21:28:55 +0530

    Email address fix

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -11,7 +11,8 @@ const syncUserCreation = inngest.createFunction(
     { event: 'clerk/user.created' },
     async ({ event }) => {
         const { id, first_name, last_name, email_addresses, image_url } = event.data
-        let username = email_addresses[0].email_addresses.split('@')[0]
+        let username = email_addresses[0].email_address.split('@')[0]
+
 
         // Check availability of username
         const user = await User.findOne({ username })
@@ -22,7 +23,7 @@ const syncUserCreation = inngest.createFunction(
 
         const userData = {
             _id: id,
-            email: email_addresses[0].email_addresses,
+            email: email_addresses[0].email_address,
             full_name: first_name + " " + last_name,
             profile_picture: image_url,
             username
@@ -40,7 +41,7 @@ const syncUserUpdation = inngest.createFunction(
     async ({ event }) => {
         const { id, first_name, last_name, email_addresses, image_url } = event.data
         const updatedUserData = {
-            email: email_addresses[0].email_addresses,
+            email: email_addresses[0].email_address,
             full_name: first_name + ' ' + last_name,
             profile_picture: image_url
         }
